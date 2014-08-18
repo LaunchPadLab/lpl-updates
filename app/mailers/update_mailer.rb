e8c9d03de36219756d9714d12b6b945e7638488b
kt32291 <Katharine322@gmail.com>
2014-08-18 16:26:02 -0500

    more changes on the email

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -4,13 +4,13 @@ class UpdateMailer < ActionMailer::Base
 
   def weekly_update(company, employee)
     @company = company
-    mail to: employee.email
+    mail to: employee.email, subject: "#{company.name} Weekly Digest"
   end
 
   def updates_request(project)
     @project = project
     @team_lead = project.team_lead
-    mail to: @team_lead.email, reply_to: @project.email, subject: "Updates this week?"
+    mail to: @team_lead.email, reply_to: @project.email, subject: "#{@project.name} Updates?"
   end
 
 end
